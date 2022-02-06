bbca51c5eb0dba257736abc86c724c5599840c99
Percy <percyher@hotmail.com>
2022-02-06 12:57:51 -0500

    Migracion Integrante faltante

--- administrador/seccion/migrar_data_beneficiario.php
+++ administrador/seccion/migrar_data_beneficiario.php
@@ -22,8 +22,8 @@ if (isset($_POST["import"])) {
   $usuarios = $db_1->select_beneficiarios(); 
 
   foreach($usuarios as $usuario) {
-    $d01 = ($usuario[7] == '') ? null : $usuario[7];
-    $d02 = ($usuario[8] == '') ? null : $usuario[8];
+    $d01 = (empty($usuario[7])) ? null : $usuario[7];
+    $d02 = (empty($usuario[8])) ? null : $usuario[8];
     $d03 = ($usuario[9] == '') ? null : $usuario[9];
     $d04 = ($usuario[10] == '') ? null : $usuario[10];
     $d05 = ($usuario[11] == '') ? null : $usuario[11];
@@ -70,7 +70,7 @@ if (isset($_POST["import"])) {
 
     // RUTINA PARA INSERTAR REGISTROS EN TABLA ENCUESTA
     //$d01 = ($usuario[1] == '') ? null : $usuario[1];
-    if ($usuario[1] == '') {
+    if (empty($usuario[1])) {
       $d01 = '2000/01/01';
       }else{
         $x = $db_1->validar_fecha_espanol($usuario[1]);
@@ -152,6 +152,128 @@ if (isset($_POST["import"])) {
 
     $db_1->Insert_derivacion_sectores($d01, $d02, $d03, $d04, $d05, $d06, $xCod); 
 
+// RUTINA PARA INSERTAR REGISTROS EN TABLA INTEGRANTES
+    // INTEGRANTE 01
+    $d01 = (empty($usuario[65])) ? null : $usuario[65];
+    $d02 = (empty($usuario[66])) ? null : $usuario[66];
+    $d03 = (empty($usuario[67])) ? null : $usuario[67];
+    $d04 = (empty($usuario[68])) ? null : $usuario[68];
+    $d05 = (empty($usuario[69])) ? null : $usuario[69];
+    if (empty($usuario[70])) {
+      $d06 = '2000/01/01';
+    }else{
+      $x = $db_1->validar_fecha_espanol($usuario[70]);
+        $d06 = ($x) ?  $usuario[70] : '2000/01/01';
+    }
+    $d07 = (empty($usuario[71])) ? null : $usuario[71];
+    $d08 = (empty($usuario[72])) ? null : $usuario[72];
+    $d09 = (empty($usuario[73])) ? null : $usuario[73];
+    $d10 = (empty($usuario[74])) ? null : $usuario[74];
+    
+    // INTEGRANTE 02
+    $d11 = (empty($usuario[75])) ? null : $usuario[75];
+    $d12 = (empty($usuario[76])) ? null : $usuario[76];
+    $d13 = (empty($usuario[77])) ? null : $usuario[77];
+    $d14 = (empty($usuario[78])) ? null : $usuario[78];
+    $d15 = (empty($usuario[79])) ? null : $usuario[79];
+    if (empty($usuario[80])) {
+      $d16 = '2000/01/01';
+    }else{
+      $x = $db_1->validar_fecha_espanol($usuario[80]);
+        $d16 = ($x) ?  $usuario[80] : '2000/01/01';
+    }
+    $d17 = (empty($usuario[81])) ? null : $usuario[81];
+    $d18 = (empty($usuario[82])) ? null : $usuario[82];
+    $d19 = (empty($usuario[83])) ? null : $usuario[83];
+    $d20 = (empty($usuario[84])) ? null : $usuario[84];
+
+    // INTEGRANTE 03
+    $d21 = (empty($usuario[85])) ? null : $usuario[85];
+    $d22 = (empty($usuario[86])) ? null : $usuario[86];
+    $d23 = (empty($usuario[87])) ? null : $usuario[87];
+    $d24 = (empty($usuario[88])) ? null : $usuario[88];
+    $d25 = (empty($usuario[89])) ? null : $usuario[89];
+    if (empty($usuario[90])) {
+      $d26 = '2000/01/01';
+    }else{
+      $x = $db_1->validar_fecha_espanol($usuario[90]);
+        $d26 = ($x) ?  $usuario[90] : '2000/01/01';
+    }
+    $d27 = (empty($usuario[91])) ? null : $usuario[91];
+    $d28 = (empty($usuario[92])) ? null : $usuario[92];
+    $d29 = (empty($usuario[93])) ? null : $usuario[93];
+    $d30 = (empty($usuario[94])) ? null : $usuario[94];
+
+    // INTEGRANTE 04
+    $d31 = (empty($usuario[95])) ? null : $usuario[95];
+    $d32 = (empty($usuario[96])) ? null : $usuario[96];
+    $d33 = (empty($usuario[97])) ? null : $usuario[97];
+    $d34 = (empty($usuario[98])) ? null : $usuario[98];
+    $d35 = (empty($usuario[99])) ? null : $usuario[99];
+    if (empty($usuario[100])) {
+      $d36 = '2000/01/01';
+    }else{
+      $x = $db_1->validar_fecha_espanol($usuario[100]);
+        $d36 = ($x) ?  $usuario[100] : '2000/01/01';
+    }
+    $d37 = (empty($usuario[101])) ? null : $usuario[101];
+    $d38 = (empty($usuario[102])) ? null : $usuario[102];
+    $d39 = (empty($usuario[103])) ? null : $usuario[103];
+    $d40 = (empty($usuario[104])) ? null : $usuario[104];
+
+    // INTEGRANTE 05
+    $d41 = (empty($usuario[105])) ? null : $usuario[105];
+    $d42 = (empty($usuario[106])) ? null : $usuario[106];
+    $d43 = (empty($usuario[107])) ? null : $usuario[107];
+    $d44 = (empty($usuario[108])) ? null : $usuario[108];
+    $d45 = (empty($usuario[109])) ? null : $usuario[109];
+    if (empty($usuario[110])) {
+      $d46 = '2000/01/01';
+    }else{
+      $x = $db_1->validar_fecha_espanol($usuario[110]);
+        $d46 = ($x) ?  $usuario[110] : '2000/01/01';
+    }
+    $d47 = (empty($usuario[111])) ? null : $usuario[111];
+    $d48 = (empty($usuario[112])) ? null : $usuario[112];
+    $d49 = (empty($usuario[113])) ? null : $usuario[113];
+    $d50 = (empty($usuario[114])) ? null : $usuario[114];
+
+    // INTEGRANTE 06
+    $d51 = (empty($usuario[115])) ? null : $usuario[115];
+    $d52 = (empty($usuario[116])) ? null : $usuario[116];
+    $d53 = (empty($usuario[117])) ? null : $usuario[117];
+    $d54 = (empty($usuario[118])) ? null : $usuario[118];
+    $d55 = (empty($usuario[119])) ? null : $usuario[119];
+    if (empty($usuario[120])) {
+      $d56 = '2000/01/01';
+    }else{
+      $x = $db_1->validar_fecha_espanol($usuario[120]);
+        $d56 = ($x) ?  $usuario[120] : '2000/01/01';
+    }
+    $d57 = (empty($usuario[121])) ? null : $usuario[121];
+    $d58 = (empty($usuario[122])) ? null : $usuario[122];
+    $d59 = (empty($usuario[123])) ? null : $usuario[123];
+    $d60 = (empty($usuario[124])) ? null : $usuario[124];
+
+    // INTEGRANTE 07
+    $d61 = (empty($usuario[125])) ? null : $usuario[125];
+    $d62 = (empty($usuario[126])) ? null : $usuario[126];
+    $d63 = (empty($usuario[127])) ? null : $usuario[127];
+    $d64 = (empty($usuario[128])) ? null : $usuario[128];
+    $d65 = (empty($usuario[129])) ? null : $usuario[129];
+    if (empty($usuario[130])) {
+      $d66 = '2000/01/01';
+    }else{
+      $x = $db_1->validar_fecha_espanol($usuario[130]);
+        $d66 = ($x) ?  $usuario[130] : '2000/01/01';
+    }
+    $d67 = (empty($usuario[131])) ? null : $usuario[131];
+    $d68 = (empty($usuario[132])) ? null : $usuario[132];
+    $d69 = (empty($usuario[133])) ? null : $usuario[133];
+    $d70 = (empty($usuario[134])) ? null : $usuario[134];    
+
+
+    $db_1->Insert_integrantes($d01, $d02, $d03, $d04, $d05, $d06, $d07, $d08, $d09, $d10, $d11, $d12, $d13, $d14, $d15, $d16, $d17, $d18, $d19, $d20, $d21, $d22, $d23, $d24, $d25, $d26, $d27, $d28, $d29, $d30, $d31, $d32, $d33, $d34, $d35, $d36, $d37, $d38, $d39, $d40, $d41, $d42, $d43, $d44, $d45, $d46, $d47, $d48, $d49, $d50, $d51, $d52, $d53, $d54, $d55, $d56, $d57, $d58, $d59, $d60, $d61, $d62, $d63, $d64, $d65, $d66, $d67, $d68, $d69, $d70, $xCod); 
   
 
 
