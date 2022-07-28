7f42686e0a6a398694e0c9de8681eb40263e3b1a
Percy <percyher@hotmail.com>
2022-07-27 21:41:09 -0500

    update script powerbi OK

--- script/004 script create_function_views.sql
+++ script/004 script create_function_views.sql
@@ -563,7 +563,7 @@ order by rp.anio_actividad;
 
 drop view IF EXISTS vista_repo_total_reach_powerbi;
 CREATE VIEW `vista_repo_total_reach_powerbi` AS
-SELECT rp.anio_actividad as Anio, rp.trimestre_actividad as Trimestre, tp.nom_tipo_proyecto as Tipo_Proyecto,t.nom_tema as Tema, st.nom_subtema as Subtema, a.nom_actividad as Actividad, rp.fecha_actividad, g.nom_genero as Sexo, r.nom_region as Region,  p.nom_proyecto as Proyecto, ad.nom_adulto as Grupo_Edad, count(rp.id_resultado_proyectos) as conteo
+SELECT rp.anio_actividad as Anio, rp.trimestre_actividad as Trimestre, tp.nom_tipo_proyecto as Tipo_Proyecto, t.nom_tema as Tema, st.nom_subtema as Subtema, a.nom_actividad as Actividad, rp.fecha_actividad, g.nom_genero as Sexo, r.nom_region as Region,  p.nom_proyecto as Proyecto, ad.nom_adulto as Grupo_Edad, count(rp.id_resultado_proyectos) as conteo
 FROM resultado_proyectos rp 
 inner join proyecto p on rp.id_proyecto = p.id_proyecto
 inner join tema t on rp.id_tema = t.id_tema
@@ -573,8 +573,8 @@ inner join tipo_proyecto tp on rp.id_tipo_proyecto = tp.id_tipo_proyecto
 inner join region r on rp.id_region = r.id_region
 inner join genero g on rp.id_genero = g.id_genero
 inner join adulto ad on rp.id_adulto = ad.id_adulto
-group by rp.anio_actividad, rp.trimestre_actividad, tp.nom_tipo_proyecto, p.nom_proyecto, t.nom_tema, st.nom_subtema, a.nom_actividad, r.nom_region, rp.fecha_actividad, g.nom_genero, ad.nom_adulto
-order by rp.anio_actividad, rp.trimestre_actividad, tp.nom_tipo_proyecto, p.nom_proyecto, t.nom_tema, st.nom_subtema, a.nom_actividad, r.nom_region, rp.fecha_actividad, rp.id_genero;
+group by rp.anio_actividad, rp.trimestre_actividad, tp.nom_tipo_proyecto, t.nom_tema, st.nom_subtema, a.nom_actividad, rp.fecha_actividad, g.nom_genero, r.nom_region, p.nom_proyecto, ad.nom_adulto
+order by rp.anio_actividad, rp.trimestre_actividad, tp.nom_tipo_proyecto, p.nom_proyecto, t.nom_tema, st.nom_subtema, a.nom_actividad, r.nom_region, rp.fecha_actividad, g.nom_genero;
 
 
 
