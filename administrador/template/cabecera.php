5f4fc7110f5cbc0f2abdd50913ff21631ede025b
Percy <percyher@hotmail.com>
2022-10-16 19:06:21 -0500

    page consultar paquetes

--- administrador/template/cabecera.php
+++ administrador/template/cabecera.php
@@ -290,7 +290,7 @@ document.addEventListener("DOMContentLoaded", function(){
 								<div class="dropdown-divider"></div>
 								<a class="dropdown-item" href="<?php echo $url."/administrador/seccion/generar_paquete.php" ?>">Enviar datos a coordinador</a>
 								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="<?php echo $url."/administrador/seccion/generar_paquete.php" ?>">Consultar estado de paquetes</a>
+								<a class="dropdown-item" href="<?php echo $url."/finanza/finanza_paquete.php" ?>">Consultar estado de paquetes</a>
 							</div>
 						</li>						
 						<a class="nav-item nav-link" href="<?php echo $url."/administrador/seccion/cerrar.php"?>">Cerrar</a>
